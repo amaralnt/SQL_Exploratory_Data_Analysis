2972689807462bab6780b086af13ad1147521f19
Natan Amaral <69180967+amaralnt@users.noreply.github.com>
2022-09-12 14:33:52 -0300

    Add files via upload

---
+++ Sample Database/Conditional Expressions/06-Import and Export.sql
@@ -0,0 +1,19 @@
+-- Creating a table that matches with the .csv file
+CREATE TABLE simple(
+a INTEGER,
+b INTEGER,
+c INTEGER
+);
+
+-- Checking the table
+SELECT *
+FROM simple;
+
+-- Copying the information from the .csv file
+-- Databases/databasename/Schemas/public/Tables/tablename (Right-click and select Import/Export Data...)
+
+-- Checking the table
+SELECT *
+FROM simple;
+
+-- Now it has the information we just imported 
